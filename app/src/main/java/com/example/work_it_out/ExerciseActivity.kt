d6ca1562e3e8a1322597a697b7794e10a77a83a6
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-05-29 09:54:50 +0530

    Timer Implemented

--- app/src/main/java/com/example/work_it_out/ExerciseActivity.kt
+++ app/src/main/java/com/example/work_it_out/ExerciseActivity.kt
@@ -2,11 +2,16 @@ package com.example.work_it_out
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.example.work_it_out.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
 
     private var binding : ActivityExerciseBinding? = null
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,9 +26,49 @@ class ExerciseActivity : AppCompatActivity() {
         }
         binding?.toolBarExercise?.setNavigationOnClickListener {
             onBackPressed()
+            setupRestView()
         }
 
-
+        setRestProgressBar()
 
     }
+
+    private fun setRestProgressBar(){
+        binding?.progressBar?.progress = restProgress
+
+        restTimer = object: CountDownTimer(10000,1000){
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                binding?.progressBar?.progress = 10-restProgress
+                binding?.textViewTimer?.text = (10-restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity,
+                    "Now we will start the Exercise",
+                    Toast.LENGTH_SHORT).show()
+            }
+
+        }.start()
+    }
+
+    private fun setupRestView(){
+        if(restTimer!=null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if(restTimer!=null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        binding = null
+    }
+
 }
