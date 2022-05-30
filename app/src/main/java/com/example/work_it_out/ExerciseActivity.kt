0d8659bca482e3e802cf0110f0535e339427e69a
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-05-30 21:54:45 +0530

    +FinishActivity Class

--- app/src/main/java/com/example/work_it_out/ExerciseActivity.kt
+++ app/src/main/java/com/example/work_it_out/ExerciseActivity.kt
@@ -1,5 +1,6 @@
 package com.example.work_it_out
 
+import android.content.Intent
 import android.media.MediaPlayer
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -20,6 +21,8 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private var restTimer: CountDownTimer? = null
     private var restProgress = 0
+    private var restTimerDuration: Long = 1
+    private var exerciseTimerDuration: Long =1
 
     private var exerciseTimer: CountDownTimer? = null
     private var exerciseProgress = 0
@@ -70,7 +73,7 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private fun setRestProgressBar(){
         binding?.progressBar?.progress = restProgress
 
-        restTimer = object: CountDownTimer(10000,1000){
+        restTimer = object: CountDownTimer(restTimerDuration*1000,1000){
             override fun onTick(millisUntilFinished: Long) {
                 restProgress++
                 binding?.progressBar?.progress = 10-restProgress
@@ -90,7 +93,7 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private fun setExerciseProgressBar(){
         binding?.progressBarExercise?.progress = exerciseProgress
 
-        exerciseTimer = object: CountDownTimer(30000,1000){
+        exerciseTimer = object: CountDownTimer(exerciseTimerDuration*1000,1000){
             override fun onTick(millisUntilFinished: Long) {
                 exerciseProgress++
                 binding?.progressBarExercise?.progress = 30-exerciseProgress
@@ -99,16 +102,15 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
             override fun onFinish() {
 
-                exerciseList!![currentExercisePosition].setIsSelected(false)
-                exerciseList!![currentExercisePosition].setIsCompleted(true)
-                exerciseAdapter!!.notifyDataSetChanged()
-
                 if(currentExercisePosition < exerciseList?.size!! - 1){
+                    exerciseList!![currentExercisePosition].setIsSelected(false)
+                    exerciseList!![currentExercisePosition].setIsCompleted(true)
+                    exerciseAdapter!!.notifyDataSetChanged()
                     setupRestView()
                 }else{
-                    Toast.makeText(this@ExerciseActivity,
-                        "Congrats Completed the 7 Min Workout",
-                        Toast.LENGTH_SHORT).show()
+                    finish()
+                    val intent = Intent(this@ExerciseActivity,FinishActivity::class.java)
+                    startActivity(intent)
                 }
             }
 
---
+++ app/src/main/java/com/example/work_it_out/FinishActivity.kt
@@ -0,0 +1,32 @@
+package com.example.work_it_out
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.work_it_out.databinding.ActivityFinishBinding
+
+class FinishActivity : AppCompatActivity() {
+
+    private var binding: ActivityFinishBinding? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFinishBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarFinishActivity)
+
+        if(supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        binding?.toolbarFinishActivity?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        binding?.btnFinish?.setOnClickListener {
+            finish()
+        }
+
+    }
+}
